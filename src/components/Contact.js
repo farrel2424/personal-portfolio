5a96edb0d0bbc9810ec73c8aa9d7c4c9dec13f84
lerraf <mfarrelramadhan244@gmail.com>
2025-06-30 22:13:51 +0700

    tryna fix email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -31,7 +31,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://farrel-r.xyz/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
