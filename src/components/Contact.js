debb5b81e4413528c02ba5f64be8c77e2fd32af6
lerraf <mfarrelramadhan244@gmail.com>
2025-07-01 10:52:06 +0700

    Feat: fixing phone sect

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -67,7 +67,7 @@ export const Contact = () => {
       // Perbaikan: 'succes' menjadi 'success' agar class CSS-nya benar
       setStatus({ success: false, message: 'Something went wrong, please try again later.' });
     }
-    // --- LOGIKA EMAILJS BERAKHIR DI SINI ---
+    
   };
 
   return (
@@ -99,7 +99,7 @@ export const Contact = () => {
                         <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                        <input type="tel" value={formDetails.phone} placeholder="Phone No." oonChange={(e) => {const value = e.target.value; if (/^[0-9]*$/.test(value)) {onFormUpdate('phone', value);}}} />
                       </Col>
                       <Col size={12} className="px-1">
                         <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
