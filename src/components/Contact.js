cb968148a4e020f95883d0cb454f8bb350ffb337
lerraf <mfarrelramadhan244@gmail.com>
2025-07-01 10:55:58 +0700

    fix:benerin

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -99,7 +99,7 @@ export const Contact = () => {
                         <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
                       </Col>
                       <Col size={12} sm={6} className="px-1">
-                        <input type="tel" value={formDetails.phone} placeholder="Phone No." oonChange={(e) => {const value = e.target.value; if (/^[0-9]*$/.test(value)) {onFormUpdate('phone', value);}}} />
+                        <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => {const value = e.target.value; if (/^[0-9]*$/.test(value)) {onFormUpdate('phone', value);}}} />
                       </Col>
                       <Col size={12} className="px-1">
                         <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
