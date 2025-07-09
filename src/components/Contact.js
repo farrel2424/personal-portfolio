aca21ea2d4d6cf4f216b999efdf836a7e52cdd7c
lerraf <mfarrelramadhan244@gmail.com>
2025-07-09 09:51:37 +0700

    revisi

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -123,7 +123,7 @@ export const Contact = () => {
 
                       <Col size={12} className="px-1" style={{marginTop: '20px'}}>
                         <ReCAPTCHA
-                          sitekey="6LfN7HUrAAAAAPUTmU61uIopLY-rEv1zNycLj9My"
+                          sitekey={REACT_APP_RECAPTCHA_SITE_KEY}
                           onChange={handleCaptchaChange}
                         />
                       </Col>
